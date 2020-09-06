62e0c8d8d7ee12ac4aeb3c859de1b08bfffcfc87
Ramón Albertí <ramon.alberti@adevinta.com>
2020-09-06 15:14:32 +0200

    Simplify scripts & clean unnecesary

--- scripts/rebuild_images.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker rmi -f eshopwebmvc
-docker rmi -f eshoppublicapi
-docker-compose build -f ../docker-compose.yml
-echo "Done rebuild"
--- scripts/service_start.sh
+++ scripts/service_start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+docker rmi -f eshopwebmvc
+docker rmi -f eshoppublicapi
+docker-compose build
+echo "Done rebuild"
 docker-compose down && docker-compose up -d
 
